24a97dbd142012102a93bb78d4208e9f80a51b73
Lito <lito@eordes.com>
2017-03-09 21:14:10 +0100

    Fixed Typo

--- src/Eusonlito/LaravelPacker/Processors/CSSmin.php
+++ src/Eusonlito/LaravelPacker/Processors/CSSmin.php
@@ -765,14 +765,14 @@ class CSSmin
      */
     private function normalize_int($size)
     {
-        if (!is_string($size)) {
+        if (is_numeric($size)) {
             return (int)$size;
         }
 
         $letter = substr($size, -1);
         $size = (int)$size;
 
-        switch ($leter) {
+        switch ($letter) {
             case 'M': case 'm': return $size * 1048576;
             case 'K': case 'k': return $size * 1024;
             case 'G': case 'g': return $size * 1073741824;
